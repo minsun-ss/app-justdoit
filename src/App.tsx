a9a3439bf2b595c2e916b49423fcd88b42347612
ssung <sungsharon@gmail.com>
2025-09-08 19:00:43 -0400

    feat: set up react app

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
+import { motion } from "framer-motion";
+import "./App.css";
+
+function HomePage() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div>
+        <button
+          className="front-page-button"
+          onClick={() => navigate("/answer")}
+        >
+          Will You Do The Thing That Needs to be Done?
+        </button>
+        <p className="footnote">a sharon production</p>
+      </div>
+    </>
+  );
+}
+
+function SplitText() {
+  return (
+    <motion.h1
+      className="animation1"
+      initial={{ y: 1000, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 1, ease: "easeInOut" }}
+    >
+      Yes?
+    </motion.h1>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/answer" element={<SplitText />} />
+      </Routes>
+    </Router>
+  );
+}
+export default App;
