b02a725b6a3f60ff45116de2d247bc21eddb02fe
Sharon Sung <sungsharon@gmail.com>
2025-10-29 15:14:31 -0400

    fix: changed the text animation so that the root body goes away

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { EaseIn, EaseIn2 } from "./animations/1.tsx";
+import { EaseIn, EaseIn2 } from "./animations/text.tsx";
 import { NoMeansYes } from "./animations/nomeansyes.tsx";
 import { HomePage } from "./animations/homepage.tsx";
 
@@ -17,7 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/answer" element={<RandomAnimationRoute />} />
-        {/*<Route path="/test" element={<NoMeansYes />} />*/}
+        <Route path="/test" element={<NoMeansYes />} />
       </Routes>
     </Router>
   );
--- src/animations/nomeansyes.tsx
+++ src/animations/nomeansyes.tsx
@@ -3,8 +3,10 @@ import { useRouteCSS } from "./homepage";
 export function NoMeansYes() {
   useRouteCSS("/src/css/nomeansyes.css");
   const body = document.body;
+  const viewportHeight = document.documentElement.clientHeight;
+
   const tilesNeeded =
-    Math.ceil(window.innerWidth / 40) * Math.ceil(window.innerHeight / 30);
+    Math.ceil(window.innerWidth / 40) * Math.ceil(viewportHeight / 30);
   for (let i = 0; i < tilesNeeded; i++) {
     const noTile = document.createElement("div");
     noTile.className = "tile";
