131519b4a3ff6e3231500ad103e9ff806faa3b6b
Sharon Sung <sungsharon@gmail.com>
2025-10-29 18:51:33 -0400

    fix: fix the css animation

--- src/animations/nomeansyes.tsx
+++ src/animations/nomeansyes.tsx
@@ -1,17 +1,23 @@
 import { useRouteCSS } from "./util";
+import { useEffect } from "react";
 
 export function NoMeansYes() {
   useRouteCSS("/src/css/nomeansyes.css");
-  const body = document.body;
-  const viewportHeight = document.documentElement.clientHeight;
+  useEffect(() => {
+    const body = document.body;
+    const viewportHeight = document.documentElement.clientHeight;
 
-  const tilesNeeded =
-    Math.ceil(window.innerWidth / 40) * Math.ceil(viewportHeight / 30);
-  for (let i = 0; i < tilesNeeded; i++) {
-    const noTile = document.createElement("div");
-    noTile.className = "tile";
-    body.appendChild(noTile);
-  }
+    const tilesNeeded =
+      Math.ceil(window.innerWidth / 40) * Math.ceil(viewportHeight / 30);
+    for (let i = 0; i < tilesNeeded; i++) {
+      const noTile = document.createElement("div");
+      noTile.className = "tile";
+      body.appendChild(noTile);
+    }
 
+    return () => {
+      document.querySelectorAll(".tile").forEach((tile) => tile.remove());
+    };
+  }, []);
   return null;
 }
