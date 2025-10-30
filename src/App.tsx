40ba46fb90a751ef10e21347483334f75cd732d7
Sharon Sung <sungsharon@gmail.com>
2025-10-29 21:26:45 -0400

    feat: added a p5.js rain animation and pinned p5 packages

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { EaseIn, EaseIn2 } from "./animations/text.tsx";
 import { NoMeansYes } from "./animations/nomeansyes.tsx";
 import { HomePage } from "./animations/homepage.tsx";
+import { Rain } from "./animations/rain.tsx";
 
-const animationList: React.ComponentType[] = [EaseIn, EaseIn2, NoMeansYes];
+const animationList: React.ComponentType[] = [EaseIn, EaseIn2, NoMeansYes, Rain];
 
 function RandomAnimationRoute() {
   const randomAnimationIdx = Math.floor(Math.random() * animationList.length);
@@ -17,7 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/answer" element={<RandomAnimationRoute />} />
-        {/*<Route path="/test" element={<NoMeansYes />} />*/}
+        <Route path="/test" element={<Rain />} />
       </Routes>
     </Router>
   );
---
+++ src/animations/rain.tsx
@@ -0,0 +1,71 @@
+import Sketch from "react-p5";
+import p5Types from "p5";
+
+export function Rain() {
+  class Raindrop {
+    x: number = 0;
+    y: number = 0;
+    velocity: number = 0;
+    width: number;
+    height: number;
+    p5: p5Types;
+
+    constructor(p5: p5Types, width: number, height: number) {
+      this.width = width;
+      this.height = height;
+      this.p5 = p5;
+      this.reset();
+    }
+
+    canvas(width: number, height: number) {
+      this.width = width;
+      this.height = height;
+    }
+
+    fall() {
+      this.y += this.velocity;
+      if (this.y >= this.height) {
+        this.reset();
+      }
+    }
+
+    display() {
+      this.p5.circle(this.x, this.y, 5);
+    }
+
+    reset() {
+      this.x = Math.random() * this.width;
+      this.y = 0;
+      this.velocity = Math.random() * 7 + 3;
+    }
+  }
+
+  const rain: Raindrop[] = [];
+
+  const setup = (p5: p5Types, canvasParentRef: Element) => {
+    const canvas_width = p5.windowWidth;
+    const num_drops = Math.floor(p5.windowWidth / 400) * 20;
+    const canvas_height = p5.windowHeight;
+    p5.createCanvas(canvas_width, canvas_height).parent(canvasParentRef);
+    for (let i = 0; i < num_drops; i++) {
+      const d = new Raindrop(p5, canvas_width, canvas_height);
+      rain.push(d);
+    }
+  };
+
+  const draw = (p5: p5Types) => {
+    p5.background(59, 59, 59);
+
+    for (let drop = 0; drop < rain.length; drop++) {
+      rain[drop].fall();
+      rain[drop].display();
+    }
+
+    p5.textSize(120);
+    p5.textFont("Courier");
+    p5.fill("white");
+    p5.text("YES", 20, p5.windowHeight - 20);
+  };
+
+  return <Sketch setup={setup} draw={draw} />;
+}
